056b18aa4ea0a05763ad55ba2b9a6bb459dc4ad3
pikesley <github@orgraphone.org>
2013-02-28 21:55:29 +0000

    Seems to work

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'sqlite'
+maintainer       'The Open Data Institute'
+maintainer_email 'tech@theodi.org'
+license          'MIT'
+description      'Installs/Configures sqlite'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
